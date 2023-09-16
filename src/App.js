62d477baa26adb16b88981d7330813c4de3ddf9f
Taegu <gmdals45@gmail.com>
2023-09-16 14:28:10 +0900

    2023-09-16 add TodoListFrame

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./component/Header";
+import TodoEditor from "./component/TodoEditor";
+import TodoList from "./component/TodoList";
 
 function App() {
   return (
     <div className="App">
         <Header />
-        <div>Todo Editor</div>
-        <div>Todo List</div>
+        <TodoEditor />
+        <TodoList />
     </div>
   );
 }
